6e9b6ad4a478ffb662395a9b4713cd20a512441e
wpbackup-git <arqamali.codified@gmail.com>
2025-04-26 03:18:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import streamlit as st
 
+import json
+
+# Only initialize once
+if not firebase_admin._apps:
+    cred = credentials.Certificate({
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": st.secrets["firebase"]["private_key"],
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    })
+    firebase_admin.initialize_app(cred)
 
 if st.button("Test Firestore Connection"):
     try:
